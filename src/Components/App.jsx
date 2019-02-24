76a82f5f6648a539efef085ef734d3b617e9b638
glen <glen_sale@yahoo.com>
2019-02-23 21:03:11 -0800

    it will create a landing page

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
-
 import './App.css';
+// import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+// import { Link } from 'react-router-dom';
+import Landing from './Landingpage';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-
-        </header>
+      <div>
+        {/* <Layout>
+          <Header>
+       
+          </Header>
+        </Layout> */}
+        <Landing />
       </div>
+
     );
   }
 }
---
+++ src/Components/Landingpage.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+
+
+class Landing extends Component {
+    render() {
+        return (
+            <div >
+                <div className="banner-text">
+                    <h1>Glen Sale</h1>
+                    <h3>Full Stack Web Developer</h3>
+
+                    <hr />
+
+                    <p>HTML/CSS | Bootstrap | JavaScript | React | React Native | NodeJS | Express | MongoDB</p>
+
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Landing;
