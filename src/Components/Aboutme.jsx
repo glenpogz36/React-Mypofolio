205286e89ce5c2efbb63f1e386424600929fa664
glen <glen_sale@yahoo.com>
2019-02-24 15:40:38 -0800

    it will add more content and fix minor bugs

--- src/Components/Aboutme.jsx
+++ src/Components/Aboutme.jsx
@@ -1,16 +1,63 @@
-import React, { Component } from 'react';
 
+
+
+import React, { Component } from 'react';
+import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
+import bg from '../Assests/img/bg.jpg'
 
 class About extends Component {
   render() {
     return (
-      <div><h1>About Page</h1>
-        <p>My name is Glen Sale born and raised from the Philippines. I move here October 9, 2005 as English is my second language.</p>
-        <p>Over the years I have learned <em>English through ESL and Ell</em> and was able to graduate from it</p>
-        <p>I have join <u>robotics clubs</u> and <u>culinary classes</u> but computer was my main interest</p>
-        <p>Currently a student epicodus to persue long term career</p></div>
+      <div style={{ color: '#fff', height: '100%', width: '100%', backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundImage: "url(" + bg + ")" }}>
+        <Grid className="contact-grid">
+          <Cell col={6}>
+            <div>
+              <p>My name is Glen Sale born and raised from the Philippines. I move here October 9, 2005 as English is my second language.</p>
+              <p>Over the years I have learned <em>English through ESL and Ell</em> and was able to graduate from it</p>
+              <p>I have join <u>robotics clubs</u> and <u>culinary classes</u> but computer was my main interest</p>
+              <p>Currently a student epicodus to persue long term career</p></div>
+            <hr />
+          </Cell>
+          <Cell col={6}>
+            <h2>Hobbies</h2>
+            <ul>
+              <li>Playing Basketball</li>
+              <li>Playing video games</li>
+              <li>Cooking</li>
+            </ul>
+            <hr />
+
+
+          </Cell>
+          <Cell col={6}>
+            <h2>Interest</h2>
+            <ul>
+              <li>Culinary</li>
+              <li>Software/Frameworks</li>
+              <li>Mechanical Engineering</li>
+            </ul>
+            <hr />
+
+
+          </Cell>
+          <Cell col={6}>
+            <h2>Experience</h2>
+            <ul>
+              <li>Green Planet</li>
+              <li>Warehouse</li>
+              <li>Swissport</li>
+            </ul>
+            <hr />
+
+
+          </Cell>
+        </Grid>
+      </div>
     )
   }
 }
 
 export default About;
+
+
+
--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -5,19 +5,53 @@ import bg from '../Assests/img/bg.jpg'
 class Contact extends Component {
   render() {
     return (
-      <div className="contact-body">
-        <img src={bg} alt="background" />
-        <h1>glen_sale@yahoo.com</h1>
-        <h2>Call : 206-910-6531</h2>
-        <h2>Why Epicodus: Epicodus allows 24/7 access computer lab providing</h2>
-        <ul>
-          <li>HTML</li>
-          <li>C#</li>
-          <li>Javasript</li>
-          <li>CSS STYLYING</li>
-          <li>React</li>
-          <li>Intership</li>
-        </ul>
+      <div style={{ color: '#fff', height: '100%', width: '100%', backgroundRepeat: 'no-repeat', backgroundSize: 'cover',  backgroundImage: "url(" + bg + ")"  }}>
+        <Grid className="contact-grid">
+          <Cell col={6}>
+            <h2>Glen Sale</h2>
+            <div ></div>
+            <p>“Not everyone fits into the typical workplace model. Some people have disabilities, familial responsibilities, or lack access to skills or transportation to get to the jobs that will pay them what they need. And some people just want to call their own shots!”</p>
+
+          </Cell>
+          <Cell col={6}>
+            <h2>Contact Me</h2>
+            <hr />
+
+            <div className="contact-list">
+              <List>
+                <ListItem>
+                  <ListItemContent style={{ fontSize: '30px', fontFamily: 'Anton' }}>
+                    <i className="fa fa-phone-square" aria-hidden="true" />
+                    (123) 456-7890
+                  </ListItemContent>
+                </ListItem>
+
+                <ListItem>
+                  <ListItemContent style={{ fontSize: '30px', fontFamily: 'Anton' }}>
+                    <i className="fa fa-fax" aria-hidden="true" />
+                    (123) 456-7890
+                  </ListItemContent>
+                </ListItem>
+
+                <ListItem>
+                  <ListItemContent style={{ fontSize: '30px', fontFamily: 'Anton' }}>
+                    <i className="fa fa-envelope" aria-hidden="true" />
+                    someone@example.com
+                  </ListItemContent>
+                </ListItem>
+
+                <ListItem>
+                  <ListItemContent style={{ fontSize: '30px', fontFamily: 'Anton' }}>
+                    <i className="fa fa-skype" aria-hidden="true" />
+                    MySkypeID
+                  </ListItemContent>
+                </ListItem>
+
+
+              </List>
+            </div>
+          </Cell>
+        </Grid>
       </div>
     )
   }
