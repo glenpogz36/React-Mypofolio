4c10622a6cc2458ccae13c98b2e0a5c9490148b8
glen <glen_sale@yahoo.com>
2019-02-23 21:28:59 -0800

    it will test if skills render with resume, if test work will render the remaining files needed to build a resume

--- src/Components/Resume.jsx
+++ src/Components/Resume.jsx
@@ -1,36 +1,46 @@
 import React, { Component } from 'react';
-
+import { Grid, Cell } from 'react-mdl';
+import Skills from './Skills';
 
 
 class Resume extends Component {
     render() {
         return (
             <div>
-                <div>
-                    <p>My name is Glen Sale born and raised from the Philippines. I move here October 9, 2005 as English is my second language.</p>
-                    <p>Over the years I have learned <em>English through ESL and Ell</em> and was able to graduate from it</p>
-                    <p>I have join <u>robotics clubs</u> and <u>culinary classes</u> but computer was my main interest</p>
-                    <p>Currently a student epicodus to persue long term career</p>
-                </div>
-                <div>
-                    <ul>
-                        <li>Family Fun Center</li>
-                        <li>Green Planet</li>
-                        <li>Red Ribbon Tukwilla</li>
-                        <li>Swissport</li>
-                    </ul>
-                    <div>
-                        <ul>
-                            <li>Mechanical Engineering</li>
-                            <li>Frameworks</li>
-                            <li>Softwares.</li>
-                            <li>Coding/Programming</li>
-                        </ul>
-                    </div>
-                </div>
+                <Grid>
+                    <Cell col={4}>
+                        <p>My name is Glen Sale born and raised from the Philippines. I move here October 9, 2005 as English is my second language.</p>
+                        <p>Over the years I have learned <em>English through ESL and Ell</em> and was able to graduate from it</p>
+                        <p>I have join <u>robotics clubs</u> and <u>culinary classes</u> but computer was my main interest</p>
+                        <p>Currently a student epicodus to persue long term career</p>
+                    </Cell>
+                    <Cell col={4}>
+
+                        <h2>Skills</h2>
+                        <Skills
+                            skill="javascript"
+                            progress={100}
+                        />
+                        <Skills
+                            skill="HTML/CSS"
+                            progress={80}
+                        />
+                        <Skills
+                            skill="NodeJS"
+                            progress={50}
+                        />
+                        <Skills
+                            skill="React"
+                            progress={25}
+                        />
+
+
+                    </Cell>
+                </Grid>
             </div>
         )
     }
 }
 
 export default Resume;
+
